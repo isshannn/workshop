226dcaf4c47459ad1afdd74af1e0d58c23c253a7
Samvas Majhi <2001samvas@gmail.com>
2023-11-01 22:46:49 +0530

    intermediate commit

--- week_01/NSE_bhav_download.py
+++ week_01/NSE_bhav_download.py
@@ -1,5 +1,8 @@
 import requests
+from zipfile import ZipFile
 from datetime import date
+import os
+from constants import BASE_DIR, NSE_HOLIDAYS, BASE_URL
 
 def check_day(argument):
     if(argument):
@@ -38,8 +41,18 @@ def check_year(argument):
 
 def file_downloader(url):
     if (url):
+        print("URL to download: [", url, "]")
         res = requests.get(url, allow_redirects=True)
         f = open("download.zip", 'wb').write(res.content)
+        # f = open(os.path.join(BASE_DIR,"download.zip"), 'wb').write(res.content)
+        # Make sure to change to the addresses below correctly for smooth running
+        # the adresses here point to the zip file downloaded and to where it's extracted
+        print(BASE_DIR)
+        print("AAA [", os.path.join(BASE_DIR,"download.zip"), "]")
+        with ZipFile(os.path.join(BASE_DIR,"download.zip"), "r") as unzip:
+            unzip.extractall(path=BASE_DIR)
+        if (os.path.exists(os.path.join(BASE_DIR,"download.zip"))):
+            os.remove(os.path.join(BASE_DIR,"download.zip"))
     return None
 
 def verify_date(argument):
@@ -52,17 +65,29 @@ def verify_date(argument):
         if(t_date > argument):
             return argument
 
-# print("Enter Date")
+print("Enter Date")
 # dd = input()
-# dd = check_day(dd)
+dd = "26"
+dd = check_day(dd)
 
-# print("Enter Month")
+print("Enter Month")
 # mm = input()
-# mm = check_month(mm)
+mm = "10"
+mm = check_month(mm)
 
-# print("Enter Year")
+print("Enter Year")
+yy = "2023"
 # yy = input()
-# yy = check_year(yy)
+yy = check_year(yy)
+
+#stores the date input by the user
+input_date = date(yy,mm,dd)
+input_date = verify_date(input_date)
 
 
 
+# tempstr to write date as str format in the url
+file_url = BASE_URL + input_date.strftime("%Y") + "/" + input_date.strftime("%b").upper() + "/cm" + input_date.strftime("%d") + input_date.strftime("%b").upper() + input_date.strftime("%Y") +"bhav.csv.zip"
+print(file_url)
+
+file_downloader(file_url)
---
+++ week_01/constants.py
@@ -0,0 +1,7 @@
+import os
+
+# contains all the holidays for last 5 years
+NSE_HOLIDAYS = []
+# base directory for downloaded bhav copies
+BASE_DIR = os.path.join(os.getcwd(),'bhav_copy')
+BASE_URL = "https://archives.nseindia.com/content/historical/EQUITIES"
