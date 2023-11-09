4a77b1e104956215214a423b77830b585646aafb
Samvas Majhi <2001samvas@gmail.com>
2023-11-09 13:11:54 +0530

    Created a new folder CODE containing all .py file

--- code/test_my_sorts.py
+++ code/test_my_sorts.py
@@ -1,5 +1,5 @@
 import unittest
-import week_02.my_sorts as local_sorts
+import my_sorts as local_sorts
 class LearnSorts(unittest.TestCase):
     def test_bubble_sort(self):
         # result = local_sorts.bubble_sort()
